6f1b6f7b8e1b868d6df7d4e726a89e05198e9bd8
Zurabi Gabrichidze <t.kelch@smf.de>
2023-01-23 16:08:58 +0100

    Tabellenansicht mit Unterpunkten (WIP)

--- Tagebuch/RenderTable.php
+++ Tagebuch/RenderTable.php
@@ -18,14 +18,21 @@ class RenderTable
 
         /*  Body    */
         echo $t . '<tbody>' . PHP_EOL;
-
-        $count = count($content);
+        $count = self::countSubItems($content);
         foreach ($content as $key => $line) {
             self::renderTableRow($line, $key, $count, $day);
         }
 
         echo $t . '</tbody>' . PHP_EOL;
         echo '</table>' . PHP_EOL;
+    }
+    private static function countSubItems(array $content): int
+    {
+        $count = 0;
+        foreach ($content as $line) {
+            $count += substr_count($line, '#');
+        }
+        return $count;
     }
 
     private static function renderTableHead(): void
@@ -46,16 +53,33 @@ class RenderTable
     private static function renderTableRow($line, $key, $count, $day): void
     {
         $t = "\t";
+        $rowspanDay = $count;
 
         echo $t . $t . '<tr>' . PHP_EOL;
         $subItems = explode('#', $line);
         $mainItem = array_shift($subItems);
 
         if ($key === 0){
-            echo $t . $t . $t . '<td rowspan="' . $count . '">' . $day . '</td>' . PHP_EOL;
+            echo $t . $t . $t . '<td class="Tag" rowspan="' . $rowspanDay . '">' . $day . '</td>' . PHP_EOL;
         }
 
-        echo $t . $t . $t . '<td>' . $mainItem . '</td>' . PHP_EOL;
+        $count = count($subItems);
+        $rowspanMain = $count;
+
+        foreach ($subItems as $subKey => $subItem){
+            if ($subKey === 0) {
+                echo $t . $t . $t . '<td class="Hauptpunkt" rowspan="' . $rowspanMain . '">' . $mainItem . '</td>' . PHP_EOL;
+            }
+
+            if ($subKey !== 0){
+                echo $t . $t . '</tr>' . PHP_EOL;
+                echo $t . $t . '<tr>' . PHP_EOL;
+            }
+
+            echo $t . $t . $t . '<td class="Unterpunkt">' . str_replace(PHP_EOL, '', $subItem) . '</td>' . PHP_EOL;
+
+        }
+
         echo $t . $t . '</tr>' . PHP_EOL;
     }
 }
