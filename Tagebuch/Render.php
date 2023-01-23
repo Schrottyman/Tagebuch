8c0b1a01792eb3f9b540f1be447c2d157374e819
Zurabi Gabrichidze <t.kelch@smf.de>
2023-01-23 10:16:19 +0100

    Dopplungen in Render.php entfernt. Daten in Tag 5 hinzugefügt.

--- Tagebuch/Render.php
+++ Tagebuch/Render.php
@@ -1,5 +1,7 @@
 <?php
-class Render {
+
+class Render
+{
     public static function header(string $name): void
     {
         echo '<h2>' . 'Tag ' . $name . '</h2>';
@@ -8,19 +10,16 @@ class Render {
     public static function content(array $content): void
     {
         echo '<ul>';
-        foreach ($content as $line){
-            if (strpos($line, '#') !== false){
-                $subItems = explode('#', $line);
-                $mainItem = array_shift($subItems);
+        foreach ($content as $line) {
+            $subItems = explode('#', $line);
+            $mainItem = array_shift($subItems);
+            echo '<li>' . $mainItem . '<ul style="margin-left: -200px">';
 
-                echo '<li>' . $mainItem . '<ul style="margin-left: -200px">';
-                foreach ($subItems as $subItem){
-                    echo '<li>' . $subItem . '</li>';
-                }
-                echo '</ul>' . '</li>';
-            } else {
-                echo '<li>' . $line . '</li>';
+            foreach ($subItems as $subItem) {
+                echo '<li>' . $subItem . '</li>';
             }
+
+            echo '</ul>' . '</li>';
         }
         echo '</ul>';
     }
